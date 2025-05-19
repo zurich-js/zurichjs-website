8a203ade16f6fa48bdc852dadc401345fa52a56f
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2025-05-19 20:00:32 +0200

    add linkedin insight tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,37 @@ export default function Document() {
           }}
         />
         {/* End Inspectlet Asynchronous Code */}
+        
+        {/* Begin LinkedIn Insight Tag */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            _linkedin_partner_id = "7172098";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `,
+          }}
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display: "none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=7172098&fmt=gif" />
+        </noscript>
+        {/* End LinkedIn Insight Tag */}
       </Head>
       <body className="antialiased">
         <Main />
